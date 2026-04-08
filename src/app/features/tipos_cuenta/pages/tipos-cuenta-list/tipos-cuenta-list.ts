dcbd1077af2a4a9e55cab2429acf0c61075559a5
RBryan05 <bryanrauda05@gmail.com>
2026-04-07 19:39:06 -0600

    Inicio de proyecto BankAdmin desarrollado en Angular

---
+++ src/app/features/tipos_cuenta/pages/tipos-cuenta-list/tipos-cuenta-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tipos-cuenta-list',
+  imports: [],
+  templateUrl: './tipos-cuenta-list.html',
+  styleUrl: './tipos-cuenta-list.css',
+})
+export class TiposCuentaList {}
